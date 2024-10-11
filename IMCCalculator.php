ca7c306de24ee27df98a6a92592de0a399354cf9
Kelian <kelian.esteve@alumni.univ-avignon.fr>
2024-10-11 11:16:32 +0200

    implémentation d'une interface graphique

---
+++ IMCCalculator.php
@@ -0,0 +1,45 @@
+<?php
+
+// Classe pour calculer l'IMC
+class IMCCalculator {
+    private $weight;
+    private $height;
+
+    // Constructeur pour initialiser le poids et la taille depuis les paramètres GET
+    public function __construct() {
+        if (isset($_GET['weight']) && isset($_GET['height'])) {
+            $this->weight = floatval($_GET['weight']);
+            $this->height = floatval($_GET['height']);
+        } else {
+            throw new Exception("Les paramètres du poids et de la taille sont requis.");
+        }
+    }
+
+    // Méthode pour calculer l'IMC
+    public function calculateIMC() {
+        if ($this->height > 0) {
+            return round($this->weight / ($this->height * $this->height), 2);
+        } else {
+            throw new Exception("La taille doit être supérieure à zéro.");
+        }
+    }
+
+    // Méthode pour afficher le résultat
+    public function displayResult() {
+        try {
+            $imc = $this->calculateIMC();
+            echo "Votre IMC est : " . $imc;
+        } catch (Exception $e) {
+            echo "Erreur : " . $e->getMessage();
+        }
+    }
+}
+
+// Crée une instance d'IMCCalculator et affiche le résultat
+try {
+    $imcCalculator = new IMCCalculator();
+    $imcCalculator->displayResult();
+} catch (Exception $e) {
+    echo "Erreur : " . $e->getMessage();
+}
+?>
